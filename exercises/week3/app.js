00339dce1facf7c83f1e83c46aaae1dc37a6853f
Gabriel <ggardiles@gmail.com>
2017-09-05 19:42:27 -0500

    improvements 2

--- exercises/week3/app.js
+++ exercises/week3/app.js
@@ -1,14 +1,27 @@
-var main = function(){
+var main = function () {
   "use strict";
   window.alert("hello world");
 
-  $(".comment-input button").on("click", function(){
-    var $newP = $("<p>");
-    var inputStr = $(".comment-input input").val();
+  $(".comment-input button").on("click", function () {
+    var $new_comment = $("<p>");
+    var $comment_text = $(".comment-input input");
 
-    $newP.text(inputStr);
-    $(".comment").append($newP);
+    if (!$comment_text.val()) {
+      window.alert("Empty Comment :(");
+      return;
+    }
+    $new_comment.text($comment_text.val());
+    $(".comment").append($new_comment);
+    $comment_text.val("");
   })
 };
+$(".comment-input input").on("keypress", function (event) {
+  var $comment_text = $(".comment-input input");
+
+  if (event.keyCode === 13){ // Check for return key press
+    $(".comment-input button").click();
+  }
+})
+
 
 $(document).ready(main);
