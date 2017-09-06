4125abd6b14341561def51b4ba68960ab7aef3b9
Gabriel <ggardiles@gmail.com>
2017-09-05 19:49:30 -0500

    improvements 3

--- exercises/week3/app.js
+++ exercises/week3/app.js
@@ -1,27 +1,33 @@
+// Custom functions
 var main = function () {
   "use strict";
   window.alert("hello world");
-
-  $(".comment-input button").on("click", function () {
-    var $new_comment = $("<p>");
-    var $comment_text = $(".comment-input input");
-
-    if (!$comment_text.val()) {
-      window.alert("Empty Comment :(");
-      return;
-    }
-    $new_comment.text($comment_text.val());
-    $(".comment").append($new_comment);
-    $comment_text.val("");
-  })
 };
+
+var addCommentFromInput = function () {
+  var $new_comment = $("<p>");
+  var $comment_text = $(".comment-input input");
+
+  if (!$comment_text.val()) {
+    window.alert("Empty Comment :(");
+    return;
+  }
+  $new_comment.text($comment_text.val());
+  $(".comment").append($new_comment);
+  $new_comment.fadeIn("fast");
+  $comment_text.val("");
+};
+
+// Events
+$(".comment-input button").on("click", addCommentFromInput);
+
 $(".comment-input input").on("keypress", function (event) {
   var $comment_text = $(".comment-input input");
 
-  if (event.keyCode === 13){ // Check for return key press
-    $(".comment-input button").click();
+  if (event.keyCode === 13) { // Check for return key press
+    addCommentFromInput();
   }
-})
+});
 
-
+// Document is ready
 $(document).ready(main);
