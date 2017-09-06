ac55600a7f679b51a0512d3b621d4d5960b7019c
Gabriel <ggardiles@gmail.com>
2017-09-05 19:22:05 -0500

    Javascript added

--- exercises/week3/app.js
+++ exercises/week3/app.js
@@ -1,6 +1,14 @@
 var main = function(){
   "use strict";
   window.alert("hello world");
+
+  $(".comment-input button").on("click", function(){
+    var $newP = $("<p>");
+    var inputStr = $(".comment-input input").val();
+
+    $newP.text(inputStr);
+    $(".comment").append($newP);
+  })
 };
 
 $(document).ready(main);
