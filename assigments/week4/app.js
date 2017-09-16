a038c339dfdec91704efe554f90d925bce8ed893
Gabriel <ggardiles@gmail.com>
2017-09-16 16:27:15 -0500

    Fixed bugs, added validation + optimizations

--- assigments/week4/app.js
+++ assigments/week4/app.js
@@ -1,11 +1,27 @@
 function handAssessor(arr) {
+
+  // Validation
+  if (arr.length > 5) {
+    throw new Error('Poker hand cannot exceed 5 cards');
+  }
+  var reason = isHandInvalid(arr);
+  if (reason) {
+    var msg = reason[0] === -1 ? 'rank: ' : 'suit: ';
+    throw new Error('Hand is not valid due to bad ' + msg + reason[1]);
+  }
+  if (isCardRepeated(arr)) {
+    throw new Error('Hand is not valid: some cards are repeated');
+  }
+
+  // Evaluation of hands with optimizations (Dont assess some impossible combinations)
   hands = []
+
   // Royal flush
   if (isRoyalFlush(arr)) {
-    hands.push(["Royal Flush", "Straight Flush", "Straight"]);
+    hands = ["Royal Flush", "Straight Flush", "Straight"];
   }
   else if (isStraightFlush(arr)) {
-    hands.push(["Straight Flush", "Straight"]);
+    hands = ["Straight Flush", "Straight"];
   }
   else if (isStraight(arr)) {
     hands.push("Straight");
@@ -13,23 +29,21 @@ function handAssessor(arr) {
 
   // Full House
   if (isFullHouse(arr)) {
-    hands.push("FullHouse");
+    hands.push("FullHouse", "Three of a kind", "Two of a kind");
   }
   // Flush
   else if (isFlush(arr)) {
     hands.push("Flush");
   }
-  // OR Four of a kind
   else if (isFour(arr)) {
     hands.push("Four of a kind");
   }
-
   // Three of a kind
-  if (isThree(arr)) {
+  else if (isThree(arr)) {
     hands.push("Three of a kind");
   }
   // Two pair
-  if (isTwo(arr)) {
+  else if (isTwo(arr)) {
     hands.push("Two of a kind");
   }
 
@@ -39,76 +53,128 @@ function handAssessor(arr) {
   } else {
     hands.forEach(function (hand) {
       console.log(hand);
-    }, this);
+    });
   }
 };
+function isHandInvalid(arr) {
+  var ranks = {
+    2: true,
+    3: true,
+    4: true,
+    5: true,
+    6: true,
+    7: true,
+    8: true,
+    9: true,
+    10: true,
+    'J': true,
+    'Q': true,
+    'K': true,
+    'A': true
+  };
+  var suits = {
+    'hearts': true,
+    'diamonds': true,
+    'clubs': true,
+    'spades': true
+  };
 
+  for (var x = 0; x < arr.length; x++) { // Check if all cards are valid
+    var card = arr[x];
+    if (!ranks[card.rank]) {
+      return [-1, card.rank + ' not in [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]'];
+    }
+    if (!suits[card.suit]) {
+      return [-2, card.suit + ' not in [hearts, diamonds, spades, clubs]'];
+    }
+  };
+  return false;
+}
+function isCardRepeated(arr) {
+  var cardArray = arr.map(function (card) { return String(card.rank) + card.suit });
+  return cardArray.some(function (card, idx) {
+    return cardArray.indexOf(card) != idx;
+  })
+}
 function isRoyalFlush(arr) {
-  arr.forEach(function(card){
-    if (isNaN(parseInt(card.rank))){
+  for (var x = 0; x < arr.length; x++) { // Check if ranks are > 10 or letters
+    var card = arr[x];
+    if (isNaN(parseInt(card.rank))) {
       continue;
     }
-    if (card.rank < 10){
+    if (card.rank < 10) {
       return false;
     }
-  });
+  };
 
-  return isStraight();
+  return isStraightFlush(arr);
 }
 function isStraightFlush(arr) {
-  return isFlush() && isStraight();
+  return isFlush(arr) && isStraight(arr);
 }
 function isStraight(arr) {
   min = 1;
   max = 14;
+  ranks = {};
 
-  arr.forEach(function(card){
+  for (var x = 0; x < arr.length; x++) {
+    var card = arr[x];
     var rank;
-    if (card.rank === "A"){
+    if (card.rank === "A") {
       rank = max === 14 ? 14 : 1;
-    }else if (card.rank === "K"){
+    } else if (card.rank === "K") {
       rank = 13;
-    }else if (card.rank === "Q"){
+    } else if (card.rank === "Q") {
       rank = 12;
-    }else if (card.rank === "J"){
+    } else if (card.rank === "J") {
       rank = 11;
+    } else {
+      rank = card.rank;
     };
 
-    // Check if rank within range of straight
-    if (rank > max || rank < min){
+    // Add to ranks set (ranks cannot be repeated)
+    if (ranks[rank]) {
       return false;
     }
+    ranks[rank] = true;
+
+    // Check if rank within range of straight
+    if (rank > max || rank < min) {
+      return false;
+    };
 
     // Get new straight range without overflow
-    tmpMin = rank - 4 > 0  ? rank - 4 : 1;
+    tmpMin = rank - 4 > 0 ? rank - 4 : 1;
     tmpMax = rank + 4 < 14 ? rank + 4 : 14;
 
     // Update straight range if more restrictive
     min = tmpMin > min ? tmpMin : min;
     max = tmpMax < max ? tmpMax : max;
-  })
+  };
 
   return true;
 }
 function isFullHouse(arr) {
   cards = {};
 
-  arr.forEach(function (card) {
+  for (var x = 0; x < arr.length; x++) {
+    var card = arr[x];
     cards[card.rank] += 1;
-    if (Object.keys(a).length > 2){
+    if (Object.keys(cards).length > 2) {
       return false;
     }
-  });
+  };
 
-  return !isFour();
+  return !isFour(arr);
 }
 function isFlush(arr) {
   suit = arr[0].suit;
-  arr.forEach(function (card) {
+  for (var x = 0; x < arr.length; x++) {
+    var card = arr[x];
     if (card.suit !== suit) {
       return false;
     }
-  });
+  };
 
   return true;
 }
@@ -124,12 +190,13 @@ function isTwo(arr) {
 function isSameRankNTimes(arr, n) {
   cards = {};
   // Iterate over cards and see if at least there are N with same rank
-  arr.forEach(function (card) {
-    cards[card.rank] += 1;
-    if (cards[card.rank] >= N) {
+  for (var x = 0; x < arr.length; x++) {
+    var card = arr[x];
+    cards[card.rank] = cards[card.rank] ? cards[card.rank] + 1 : 1;
+    if (cards[card.rank] >= n) {
       return true;
     }
-  });
+  };
 
   return false;
 }
---
+++ assigments/week4/test.js
@@ -0,0 +1,207 @@
+
+var handRoyalFlush = [
+  {
+    rank: 'A',
+    suit: 'hearts'
+  },
+  {
+    rank: 10,
+    suit: 'hearts'
+  },
+  {
+    rank: 'K',
+    suit: 'hearts'
+  },
+  {
+    rank: 'J',
+    suit: 'hearts'
+  },
+  {
+    rank: 'Q',
+    suit: 'hearts'
+  },
+]
+
+var handStraightFlush = [
+  {
+    rank: 9,
+    suit: 'hearts'
+  },
+  {
+    rank: 10,
+    suit: 'hearts'
+  },
+  {
+    rank: 'K',
+    suit: 'hearts'
+  },
+  {
+    rank: 'J',
+    suit: 'hearts'
+  },
+  {
+    rank: 'Q',
+    suit: 'hearts'
+  },
+]
+var handFour = [
+  {
+    rank: 2,
+    suit: 'diamonds'
+  },
+  {
+    rank: 2,
+    suit: 'hearts'
+  },
+  {
+    rank: 2,
+    suit: 'spades'
+  },
+  {
+    rank: 2,
+    suit: 'clubs'
+  },
+  {
+    rank: 3,
+    suit: 'spades'
+  },
+]
+
+var handFull = [
+  {
+    rank: 2,
+    suit: 'diamonds'
+  },
+  {
+    rank: 2,
+    suit: 'hearts'
+  },
+  {
+    rank: 2,
+    suit: 'spades'
+  },
+  {
+    rank: 3,
+    suit: 'clubs'
+  },
+  {
+    rank: 3,
+    suit: 'spades'
+  },
+]
+
+var handStraight = [
+  {
+    rank: 2,
+    suit: 'diamonds'
+  },
+  {
+    rank: 3,
+    suit: 'hearts'
+  },
+  {
+    rank: 4,
+    suit: 'spades'
+  },
+  {
+    rank: 5,
+    suit: 'clubs'
+  },
+  {
+    rank: 6,
+    suit: 'spades'
+  },
+]
+
+var handBust = [
+  {
+    rank: 2,
+    suit: 'diamonds'
+  },
+  {
+    rank: 5,
+    suit: 'hearts'
+  },
+  {
+    rank: 10,
+    suit: 'spades'
+  },
+  {
+    rank: 'J',
+    suit: 'clubs'
+  },
+  {
+    rank: 'A',
+    suit: 'spades'
+  },
+]
+
+var handFlush = [
+  {
+    rank: 2,
+    suit: 'spades'
+  },
+  {
+    rank: 5,
+    suit: 'spades'
+  },
+  {
+    rank: 10,
+    suit: 'spades'
+  },
+  {
+    rank: 'J',
+    suit: 'spades'
+  },
+  {
+    rank: 'A',
+    suit: 'spades'
+  },
+]
+
+var handTwo = [
+  {
+    rank: 2,
+    suit: 'spades'
+  },
+  {
+    rank: 2,
+    suit: 'hearts'
+  },
+  {
+    rank: 10,
+    suit: 'spades'
+  },
+  {
+    rank: 'J',
+    suit: 'spades'
+  },
+  {
+    rank: 'A',
+    suit: 'spades'
+  },
+]
+
+var handThree = [
+  {
+    rank: 2,
+    suit: 'spades'
+  },
+  {
+    rank: 2,
+    suit: 'hearts'
+  },
+  {
+    rank: 2,
+    suit: 'clubs'
+  },
+  {
+    rank: 'J',
+    suit: 'spades'
+  },
+  {
+    rank: 'A',
+    suit: 'spades'
+  },
+]
+
